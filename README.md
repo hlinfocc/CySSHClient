8d55d55cac8b60d8c8e5460760b67b532237f332
hlinfocc <kf39@hlinfo.net>
2018-11-23 15:23:58 +0800

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CySSHClient
 基于Linux系统终端ssh命令的ssh客户端，方便在Linux系统使用ssh命令登录主机，不用记很多主机地址,直接一个命令就可以登录远程主机，省去输入主机地址的麻烦！
 ## 使用示例
-1.	运行install.sh安装程序，安装完毕后的命令是CySSH（此命令可以在install.sh中修改）。
-2.	了解参数运行`CySSH -h`，`CySSH -add`增加一条记录，`CySSH -d`删除一条记录
-3.	运行CySSH 选择主机，输入密码即可登录，如果是免密码SSH证书可直接登录。  
+1.	运行install.sh安装程序，安装完毕后的命令是cyssh（此命令可以在install.sh中修改）。
+2.	了解参数运行`cyssh -h`，`cyssh -add`增加一条记录，`cyssh -d`删除一条记录
+3.	运行cyssh 选择主机，输入密码即可登录，如果是免密码SSH证书可直接登录。  
--- install.sh
+++ install.sh
@@ -8,16 +8,16 @@ dqwz=$(dirname `readlink -f $0`)
 echo "Install CySSHClient Begin"
 if [ ! -d "/usr/local/CySSHClient" ]
 then
-	cp -r ${dqwz}/CySSHClient /usr/local/
+	\cp -r ${dqwz}/CySSHClient /usr/local/
 	chmod +x /usr/local/CySSHClient/CySSHClient.py
 	echo "cp ${dqwz}/CySSHClient /usr/local/"
 else
 	echo "the CySSHClient already exists"
 	echo "cp -rf ${dqwz}/CySSHClient/CySSHClient.py /usr/local/CySSHClient/"
-	cp -rf ${dqwz}/CySSHClient/CySSHClient.py /usr/local/CySSHClient/
+	\cp -rf ${dqwz}/CySSHClient/CySSHClient.py /usr/local/CySSHClient/
 	echo "The /usr/local/CySSHClient/cyssh.db no update!"
 fi
 
-ln -sf /usr/local/CySSHClient/CySSHClient.py /usr/bin/CySSH
+ln -sf /usr/local/CySSHClient/CySSHClient.py /usr/bin/cyssh
 echo "install CySSHClient success!"
 echo "you can use command 'CySSH' or use command 'CySSH -h' show help"
